304eb4ad7b85133a38473eb9ad37965e5d812a9c
PanduPatpat <winataekapandu@gmail.com>
2016-04-11 00:26:04 +0700

    Epw commit tips 2

--- app/src/main/java/com/android/mirzaadr/pakanku/Adapter/ListTipsAdapter.java
+++ app/src/main/java/com/android/mirzaadr/pakanku/Adapter/ListTipsAdapter.java
@@ -2,6 +2,8 @@ package com.android.mirzaadr.pakanku.Adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,11 +14,22 @@ import android.widget.TextView;
 
 import com.android.mirzaadr.pakanku.ArticleView;
 import com.android.mirzaadr.pakanku.Info_tips;
-import com.android.mirzaadr.pakanku.Internet.ImageLoader;
 import com.android.mirzaadr.pakanku.R;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.display.CircleBitmapDisplayer;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+
 
 public class ListTipsAdapter extends BaseAdapter {
 
@@ -24,14 +37,25 @@ public class ListTipsAdapter extends BaseAdapter {
 	Context context;
 	LayoutInflater inflater;
 	ArrayList<HashMap<String, String>> data;
-	ImageLoader imageLoader;
+	//ImageLoader imageLoader;
 	HashMap<String, String> resultp = new HashMap<String, String>();
+	private DisplayImageOptions options;
+	private ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
+	ImageLoader imageLoader = ImageLoader.getInstance();
 
 	public ListTipsAdapter(Context context,
 						   ArrayList<HashMap<String, String>> arraylist) {
 		this.context = context;
 		data = arraylist;
-		imageLoader = new ImageLoader(context);
+		//imageLoader = new ImageLoader(context);
+		options = new DisplayImageOptions.Builder()
+				.showImageOnLoading(R.drawable.ic_stub)
+				.showImageForEmptyUri(R.drawable.ic_empty)
+				.showImageOnFail(R.drawable.ic_error)
+				.cacheInMemory(true)
+				.cacheOnDisk(true)
+				.considerExifParams(true)
+				.build();
 	}
 
 	@Override
@@ -63,12 +87,12 @@ public class ListTipsAdapter extends BaseAdapter {
 		// Get the position
 		resultp = data.get(position);
 
-		// Locate the TextViews in listview_item.xml
+
 		rank = (TextView) itemView.findViewById(R.id.rank);
 		country = (TextView) itemView.findViewById(R.id.country);
 		population = (TextView) itemView.findViewById(R.id.population);
 
-		// Locate the ImageView in listview_item.xml
+
 		flag = (ImageView) itemView.findViewById(R.id.flag);
 
 		// Capture position and set results to the TextViews
@@ -77,7 +101,12 @@ public class ListTipsAdapter extends BaseAdapter {
 		population.setText(resultp.get(Info_tips.LINK));
 		// Capture position and set results to the ImageView
 		// Passes flag images URL into ImageLoader.class
-		imageLoader.DisplayImage(resultp.get(Info_tips.GAMBAR), flag);
+		//BaseActivity.imageLoader.init(ImageLoaderConfiguration.createDefault(getBaseCont‌​ext()))
+
+		imageLoader.init(ImageLoaderConfiguration.createDefault(context));
+		//imageLoader.displayImage(resultp.get(MainActivity.GAMBAR), flag);
+		imageLoader.displayImage(resultp.get(Info_tips.GAMBAR), flag, options, animateFirstListener);
+		//imageLoader.DisplayImage(resultp.get(MainActivity.GAMBAR), flag);
 		// Capture ListView item click
 		itemView.setOnClickListener(new OnClickListener() {
 
@@ -92,11 +121,30 @@ public class ListTipsAdapter extends BaseAdapter {
 				intent.putExtra("deskripsi", resultp.get(Info_tips.DESKRIPSI));
 				// Pass all data population
 				intent.putExtra("link",resultp.get(Info_tips.LINK));
-				// Start SingleItemView Class
+				// Pass all data flag
+				intent.putExtra("gambar", resultp.get(Info_tips.GAMBAR));
+
 				context.startActivity(intent);
 
 			}
 		});
 		return itemView;
+	}
+
+	private static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {
+
+		static final List<String> displayedImages = Collections.synchronizedList(new LinkedList<String>());
+
+		@Override
+		public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+			if (loadedImage != null) {
+				ImageView imageView = (ImageView) view;
+				boolean firstDisplay = !displayedImages.contains(imageUri);
+				if (firstDisplay) {
+					FadeInBitmapDisplayer.animate(imageView, 500);
+					displayedImages.add(imageUri);
+				}
+			}
+		}
 	}
 }
