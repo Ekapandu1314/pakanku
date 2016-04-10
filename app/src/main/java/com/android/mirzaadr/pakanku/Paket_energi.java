a0c67aa6509fd47253824261b71997da7f0ebd4c
Mirzaadr <mirzaadr34@yahoo.com>
2016-04-10 17:13:14 +0700

    commit 2 by mirzaadr

--- app/src/main/java/com/android/mirzaadr/pakanku/Paket_energi.java
+++ app/src/main/java/com/android/mirzaadr/pakanku/Paket_energi.java
@@ -8,6 +8,6 @@ public class Paket_energi extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_paket_energi);
+        setContentView(R.layout.paket_energi);
     }
 }
